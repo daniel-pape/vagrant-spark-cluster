0870934a700ea5e07c0638674e4503f4644a2012
daniel-pape <daniel-pape@users.noreply.github.com>
2016-04-06 10:23:28 +0200

    Update submit-script-pi.sh
    Removed comment.

--- scripts/submit-script-pi.sh
+++ scripts/submit-script-pi.sh
@@ -5,6 +5,3 @@ spark-submit \
 --master spark://192.168.33.100:7077  \
 --conf spark.eventLog.enabled=true \
 /vagrant/jars/spark-pi-example-1.0.jar 10
-
-#--class org.apache.spark.examples.SparkPi \
-#$SPARK_HOME/lib/spark-examples-1.6.0-hadoop2.6.0.jar 10
